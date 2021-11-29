96f9fbfe8ff7d7bf02ccadfbc9108fb4c1cf9e48
Redseb <mikolajzyzanski@gmail.com>
2021-11-29 16:45:05 +0100

    Implemented Teacher

--- BYT-9-10/Models/Teacher.cs
+++ BYT-9-10/Models/Teacher.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BYT_9_10.Models
 {
-    internal class Teacher
+    internal class Teacher : Account
     {
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
+        public string emailPrefix { get; set; }
+        private List<Class> classes;
+
+
+        public override bool login()
+        {
+            //check this.emailprefix and this.password
+            return true;
+        }
+        public List<Class> getListOfClasses()
+        {
+            return classes;
+        }
     }
 }
