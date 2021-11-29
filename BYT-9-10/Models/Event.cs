136242f9d4c441d8ffe7f30282d65c8d4b722571
Redseb <mikolajzyzanski@gmail.com>
2021-11-29 16:50:15 +0100

    Implemented Event

--- BYT-9-10/Models/Event.cs
+++ BYT-9-10/Models/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace BYT_9_10.Models
 {
     internal class Event
     {
+        [Required]
+        public int eventId { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public DateTime startDateTime { get; set; }
+        [Required]
+        public DateTime endDateTime { get; set; }
+        public bool isCancelled { get; set; }
+
+        public void cancelEvent() { 
+            this.isCancelled = true;
+        }
+
+        public void changeStartTime(DateTime newDateTime) {
+            this.startDateTime = newDateTime;
+        }
     }
 }
