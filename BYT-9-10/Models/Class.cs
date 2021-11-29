8a219ebf142cfc6bf5f180315c269cc3863e80fa
Redseb <mikolajzyzanski@gmail.com>
2021-11-29 16:56:43 +0100

    Implemented Class

--- BYT-9-10/Models/Class.cs
+++ BYT-9-10/Models/Class.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BYT_9_10.Models
 {
-    internal class Class
+    internal class Class : Event
     {
+        [Required]
+        [StringLength(4, MinimumLength =4)] //ie. A122
+        public string roomNumber { get; set; }
+        [Required]
+        public bool isLecture { get; set; }
+        [Required]
+        [StringLength(3, MinimumLength =3)] //ie. 14c
+        public string group { get; set; }
+        private List<Student> students;
+
+        public List<Student> getListOfStudents()
+        {
+            return students;
+        }
+
+        public static List<Class> getAllclasses()
+        {
+            return new List<Class> { }; //Would somehow get every class
+        }
+
+        public void viewRouteTo() {
+            //Would interact with the screen to display the route
+            return;
+        }
+
+        public void addStudent(Student newStudent) { 
+            this.students.Add(newStudent);
+        }
+
+        public void removeStudent(Student newStudent) { 
+            this.students.Remove(newStudent);
+        }
     }
 }
