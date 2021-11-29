ec4e616cdd67519e3956c1de094b7b5092d4b386
Redseb <mikolajzyzanski@gmail.com>
2021-11-29 16:41:01 +0100

    Removed unnecessary constructor and password field from Student

--- BYT-9-10/Models/Student.cs
+++ BYT-9-10/Models/Student.cs
@@ -9,14 +9,9 @@ namespace BYT_9_10.Models
     internal class Student : Account
     {
         private int studentNumber { get; set; }
-        private string password { get; set; }
         private List<Class> classes;
         private List<Event> events;
 
-        public Student(int studentNumber, string password) {
-            this.studentNumber = studentNumber;
-            this.password = password;
-        }
         public override bool login()
         {
             //Check this.studentNumber and this.password
