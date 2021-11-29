0479d020524df3c66757156317495296126c4827
mgrabv <s20504@pjwstk.edu.pl>
2021-11-29 16:25:04 +0100

    Added content to class Student.

--- BYT-9-10/Models/Student.cs
+++ BYT-9-10/Models/Student.cs
@@ -8,5 +8,30 @@ namespace BYT_9_10.Models
 {
     internal class Student
     {
+        private int studentNumber { get; set; }
+        private string password { get; set; }
+        private List<Class> classes { set; }
+        private List<Event> events { set; }
+
+        public Student(int studentNumber, string password) {
+            this.studentNumber = studentNumber;
+            this.password = password;
+        }
+
+        public void login(int studentNumber, string password) { //Returns true if login successfull.
+            return true;
+        }
+
+        public List<Class> getListOfClasses() {
+            return classes;
+        }
+
+        public List<Event> getListOfEvents() {
+            return events;
+        }
+
+        public static getAllStudentsFailingSubjects() { //Returns list of all students failing subjects.
+            return new List<Student>{};
+        }
     }
 }
