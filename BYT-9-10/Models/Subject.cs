dc9d26102dc649018c493481c3ced00327696754
Redseb <mikolajzyzanski@gmail.com>
2021-11-30 12:36:45 +0100

    Implemented Subject

--- BYT-9-10/Models/Subject.cs
+++ BYT-9-10/Models/Subject.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BYT_9_10.Models
 {
+    public enum PassMode { 
+        Exam,
+        Pass,
+        ExamPass
+    }
     internal class Subject
     {
+        [Required]
+        [StringLength(50, MinimumLength =5)]
+        public string subjectName { get; set; }
+        [Required]
+        [StringLength(3, MinimumLength = 3)]
+        public string subjectShortening { get; set; }
+        [Required]
+        public PassMode passMode { get; set; }
+
+        public Double getPassRate() { //Returns passrate as a percentage
+            return 0.5;
+        }
     }
 }
