096fdd90e563b38c4062e9dff410376a5a5eeaf9
Redseb <mikolajzyzanski@gmail.com>
2021-11-30 12:45:57 +0100

    Implemented Message

--- BYT-9-10/Models/Message.cs
+++ BYT-9-10/Models/Message.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BYT_9_10.Models {
     internal class Message {
+        [Required]
+        public int messageId { get; set; }
+        [Required]
+        [StringLength(1000,MinimumLength = 1)]
+        public string body { get; set; }
+        [Required]
+        public DateTime sendDateTime { get; set; }
+        [Required]
+        public DateTime readDateTime { get; set; }
+        [Required]
+        public Student sender { get; set; }
+        [Required]
+        public Student receiver { get; set; }
+
+        public static bool sendMessage(string studentNumberSender, string studentNumberReceiver, string body) {
+            //Sends a message to studentNumber
+            return true;
+        }
     }
 }
