207bbcf38d5057932719162b0ec616c66343c2ab
mgrabv <s20504@pjwstk.edu.pl>
2021-11-30 12:54:15 +0100

    Added content to class StudentSubject.

--- BYT-9-10/Models/StudentSubject.cs
+++ BYT-9-10/Models/StudentSubject.cs
@@ -6,5 +6,25 @@ using System.Threading.Tasks;
 
 namespace BYT_9_10.Models {
     internal class StudentSubject {
+
+        public enum Grade {
+            Insufficient = 2.0,
+            Sufficient = 3.0,
+            Satisfactory = 3.5,
+            Good = 4.0,
+            VeryGood = 4.5,
+            Excellent = 5.0
+        }
+
+        [Required]
+        public Student student { get; set; }
+
+        [Required]
+        public Subject subject { get; set; }
+
+        [Required]
+        public Grade grade { get; set; }
+
+        public DateTime dateUpdated { get; set; }
     }
 }
