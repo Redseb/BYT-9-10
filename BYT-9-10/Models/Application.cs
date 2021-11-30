a907bb10860b24212d5cc18d4e32b1d578949beb
mgrabv <s20504@pjwstk.edu.pl>
2021-11-30 12:40:32 +0100

    Added content to Applicaion class.

--- BYT-9-10/Models/Application.cs
+++ BYT-9-10/Models/Application.cs
@@ -6,5 +6,31 @@ using System.Threading.Tasks;
 
 namespace BYT_9_10.Models {
     internal class Application {
+
+        [Required]
+        public string applicationId { get; set; }
+
+        [Required]
+        public DateTime sendDate { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string applicationBody { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string replyMessage { get; set; }
+
+        public bool isCancelled { get; set; }
+
+
+        public void cancelApplication() {
+            isCancelled = true;
+        }
+
+        public static List<Application> getListOfApplications(DateTime minSendDate) {
+            //Returns list of all Applications with sendDate later than minSendDate.
+            return new List<Application> {};
+        }
     }
 }
