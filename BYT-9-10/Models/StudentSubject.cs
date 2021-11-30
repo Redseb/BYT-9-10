8a1c9e62b3fb19860e0ea6d493fd7bbef6b731e1
Redseb <mikolajzyzanski@gmail.com>
2021-11-30 13:17:25 +0100

    Fixed enum in StudentSubject by changing it to a struct

--- BYT-9-10/Models/StudentSubject.cs
+++ BYT-9-10/Models/StudentSubject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,13 +8,13 @@ using System.Threading.Tasks;
 namespace BYT_9_10.Models {
     internal class StudentSubject {
 
-        public enum Grade {
-            Insufficient = 2.0,
-            Sufficient = 3.0,
-            Satisfactory = 3.5,
-            Good = 4.0,
-            VeryGood = 4.5,
-            Excellent = 5.0
+        public struct Grade {
+            public const double Insufficient = 2.0;
+            public const double Sufficient = 3.0;
+            public const double Satisfactory = 3.5;
+            public const double Good = 4.0;
+            public const double VeryGood = 4.5;
+            public const double Excellent = 5.0;
         }
 
         [Required]
