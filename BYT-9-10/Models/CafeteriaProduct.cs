2ab24724586ff035a517bf36509461cd168db875
Redseb <mikolajzyzanski@gmail.com>
2021-11-30 12:50:29 +0100

    Implemented CaferteriaProduct

--- BYT-9-10/Models/CafeteriaProduct.cs
+++ BYT-9-10/Models/CafeteriaProduct.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BYT_9_10.Models {
     internal class CafeteriaProduct {
+        [Required]
+        public int productID { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public Double price { get; set; }
+        [Required]
+        public DateTime bestBeforeDate { get; set; }
+
+        public void changePrice(double newPrice) {
+            this.price = newPrice;
+        }
+
     }
 }
