e81dcec37c960925e7e63d37909039f88fa1f221
Tomasz Kikuła <tomasz.kikula@gmail.com>
2021-11-30 19:14:40 +0100

    Added test solution

---
+++ AppTests/AccountTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BYT_9_10.Models;
+
+namespace AppTests
+{
+    [TestClass]
+    public class AccountTest
+    {
+
+        [TestMethod]
+        public void NameTest()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
