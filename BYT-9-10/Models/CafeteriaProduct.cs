9cffa10d0ce9cc7a608cd5e8029866b96372537b
Redseb <mikolajzyzanski@gmail.com>
2021-12-08 16:22:51 +0100

    Added validation and tests to CafeteriaProduct

--- BYT-9-10/Models/CafeteriaProduct.cs
+++ BYT-9-10/Models/CafeteriaProduct.cs
@@ -9,17 +9,56 @@ namespace BYT_9_10.Models
 {
     public class CafeteriaProduct 
     {
+        private int _productId;
+        private Double _price;
+        private DateTime _bestBeforeDate;
         [Required]
-        public int ProductID { get; set; }
+        public int ProductID {
+            get {
+                return _productId;
+            }
+            set {
+                if (value < 0) {
+                    throw new ValidationException();
+                }
+                _productId = value;
+            }
+        }
         [Required]
         [Range(0.01, double.MaxValue)]
-        public Double Price { get; set; }
+        public Double Price {
+            get {
+                return _price;
+            }
+            set {
+                if (value < 0.01) {
+                    throw new ValidationException();
+                }
+                _price = value;
+            }
+        }
         [Required]
-        public DateTime BestBeforeDate { get; set; }
+        public DateTime BestBeforeDate {
+            get {
+                return _bestBeforeDate;
+            }
+            set {
+                if (value < DateTime.Now) { //Don't accept past best before dates
+                    throw new ValidationException();
+                }
+                _bestBeforeDate = value;
+            }
+        }
+
+        public CafeteriaProduct(int productId, Double price, DateTime bestBeforeDate) {
+            ProductID = productId;
+            Price = price;
+            BestBeforeDate = bestBeforeDate;
+        }
 
         public void ChangePrice(double newPrice) 
         {
-            this.Price = newPrice;
+            Price = newPrice;
         }
 
     }
